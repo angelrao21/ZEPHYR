9453a67e672d78ef627eb156f1a31d5c2126e7f1
harsh23cse <harsh.23cse@bmu.edu.in>
2025-05-08 02:22:17 +0530

    D CAFE MESS DONE

---
+++ D-cafe.js
@@ -0,0 +1,9 @@
+function copyLocation() {
+    const location = document.getElementById("locationText").textContent;
+    navigator.clipboard.writeText(location).then(() => {
+        alert("Location copied to clipboard!");
+    }).catch((err) => {
+        alert("Failed to copy location.");
+        console.error(err);
+    });
+}
